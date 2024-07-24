2e9fcffa2b43bd9f69dce9c796b372d67acd91ed
Agnes Gran <agnesgrahn95@gmail.com>
2024-07-24 19:10:48 +0000

    change incorrect naming and spelling mistakes

--- gallery/views.py
+++ gallery/views.py
@@ -2,17 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Art
 
-# Create your views here.
-
 def gallery_home(request):
     return HttpResponse("Hello, Gallery!")
 
 def home(request):
     return HttpResponse("Welcome to Artful!")
-    
 
 def art_gallery(request):
     artworks = Art.objects.all()
     return render(request, 'gallery/art_gallery.html', {'artworks': artworks})
-
-
