d37386bffaeb553f3d0d6a47fe54ce8bd695dbca
Agnes Gran <agnesgrahn95@gmail.com>
2024-07-25 18:50:29 +0000

    greate home.html template

--- artful/urls.py
+++ artful/urls.py
@@ -17,12 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from gallery.views import home
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gallery/', include('gallery.urls')),
-    path('', home, name='home'),
     path('accounts/', include('allauth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', home, name='home'),
 ]
+
