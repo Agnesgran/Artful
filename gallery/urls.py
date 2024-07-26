d6d0f1245954adf206250e0faffd8e2b5cd3f402
Agnes Gran <agnesgrahn95@gmail.com>
2024-07-26 22:16:25 +0000

    Add url and template for uploading new artworks

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,6 +6,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
     path('gallery/', views.art_gallery, name='art_gallery'),
+    path('upload/', views.upload_art, name='upload_art'),
     path('profile/', views.profile, name='profile'),
     path('update_profile/', views.update_profile, name='update_profile'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
